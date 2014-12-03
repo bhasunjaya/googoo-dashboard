84141056916c099f305600a8d019f798a6da2edc
anwar <anwar@anwarnur>
2014-12-03 12:27:56 +0700

    print_ playlist query

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -18,7 +18,7 @@ class ApiController extends BaseController {
                 INNER JOIN `music_interests` ON `music_interests`.`fb_user_id`=`listeners`.`facebook_id`
                 INNER JOIN `artists` ON `artists`.`id`=`music_interests`.`artist_id`
             WHERE 
-                `listeners`.`program_id` = ?
+                `listeners`.`program_id` = ".$program->id."
                 AND DATE(listeners.created_at) = ?
                 AND `artists`.`name` IS NOT NULL
                 AND artists.id NOT IN (SELECT artist_id FROM artist_exception)
